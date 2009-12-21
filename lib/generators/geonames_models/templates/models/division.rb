d5d0ba7f73f63b264b68e77acefa0e2fd27138fa
amuino <amuino@gmail.com>
2009-12-21 09:24:17 +0100

    - fix getting containers when no administrative divisions are found (i.e. for ADM1 divisions) - cache containers at the instance level

--- lib/generators/geonames_models/templates/models/division.rb
+++ lib/generators/geonames_models/templates/models/division.rb
@@ -7,14 +7,15 @@ class Division < ActiveRecord::Base
   # The first position in the array is the +country+ and up to 3 more positions
   # can contain the ADM1, ADM2 and ADM3 divisions containing this one
   def containers
+    return @containers unless @containers.nil?
     container_codes = []
     codes = code.split('|')
     container_codes << (codes = codes[0..-2]).join('|') while codes.size > 1
-    puts container_codes.pretty_inspect
     # first is country code
-    containers = [country]
+    @containers = [country]
     # second, third can be parent administrative division if not nil
     # NOTE container codes is like: ["ES|58|PO", "ES|58", "ES"] (country is last)
-    containers += Division.find_all_by_code container_codes[0..-2], :order => :level if container_codes.size > 1
+    @containers += Division.find_all_by_code container_codes[0..-2], :order => :level if container_codes.size > 1
+    return @containers
   end
 end
